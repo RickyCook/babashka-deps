5ce57ed3b67b5004acda1cb0a855a0524c1043d6
Ricky Cook <mail@thatpanda.com>
2017-01-17 11:58:44 +1100

    gdone alias uses default branch

--- files/zshrc.d/30-aliases.zsh
+++ files/zshrc.d/30-aliases.zsh
@@ -1,3 +1,3 @@
 alias sshu="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 alias scpu="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-alias gdone='GDONEWITH=$(git rev-parse --abbrev-ref HEAD); git checkout master && git pull && git branch -d $GDONEWITH'
+alias gdone='GDONEWITH=$(git rev-parse --abbrev-ref HEAD); GDEFAULTBR=$(git name-rev --name-only $(git rev-parse origin/HEAD)); git checkout "$GDEFAULTBR" && git pull && git branch -d "$GDONEWITH"'
