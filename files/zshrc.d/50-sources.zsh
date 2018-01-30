4c9ae7682e18b950ce5edde4d41c90798c19cc21
Ricky Cook <mail@thatpanda.com>
2018-01-30 18:05:36 +1100

    Remove google cloud SDK

--- files/zshrc.d/50-sources.zsh
+++ files/zshrc.d/50-sources.zsh
@@ -3,8 +3,8 @@ try_source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlightin
 try_source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 try_source ~/.travis/travis.sh
 try_source ~/.fzf.zsh
-try_source ~/google-cloud-sdk/path.zsh.inc
-try_source ~/google-cloud-sdk/completion.zsh.inc
+#try_source ~/google-cloud-sdk/path.zsh.inc
+#try_source ~/google-cloud-sdk/completion.zsh.inc
 try_source ~/.iterm2_shell_integration.zsh
 try_source /usr/local/bin/aws_zsh_completer.sh
 
