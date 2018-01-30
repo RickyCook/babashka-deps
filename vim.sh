845e75269a12e834fbe746f73d0067de07bbd5d5
Ricky Cook <mail@thatpanda.com>
2018-01-30 19:27:55 +1100

    Install vim plugins

--- vim.sh
+++ vim.sh
@@ -1,14 +1,27 @@
 function vim() {
     requires vim_installed
+    requires macvim_installed
     requires vim_rc
+    requires vim_plugins_installed
 }
 function vim_installed() {
     install_package vim
     process
+}
+function macvim_installed() {
     install_package macvim
     process
 }
 function vim_rc() {
     file_linked ~/.vimrc
+    process
+}
+function vim_plugins_installed() {
+    function is_met() {
+        [ -d ~/.vim/plugged ]
+    }
+    function meet() {
+        /usr/local/bin/vim --not-a-term -c 'PlugInstall | qa!'
+    }
     process
 }
