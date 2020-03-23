eb058042b31f9bc91ee075746ac443bf4644f562
Ricky Cook <mail@thatpanda.com>
2020-03-23 17:15:35 +1100

    Add extra casks

--- casks.sh
+++ casks.sh
@@ -1,6 +1,10 @@
 function casks() {
     requires fonts_tapped
     requires font_fira_code_installed
+    requires docker_installed
+    requires alfred_installed
+    requires little_snitch_installed
+    requires pdvorak_installed
 }
 function fonts_tapped() {
     brew_tapped homebrew/cask-fonts
@@ -10,5 +14,21 @@ function font_fira_code_installed() {
     cask_install font-firacode-nerd-font
     process
     cask_install font-firacode-nerd-font-mono
+    process
+}
+function docker_installed() {
+    cask_install docker
+    process
+}
+function alfred_installed() {
+    cask_install alfred
+    process
+}
+function little_snitch_installed() {
+    cask_install little-snitch
+    process
+}
+function pdvorak_installed {
+    cask_install programmer-dvorak
     process
 }
