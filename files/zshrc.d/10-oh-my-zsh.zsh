edcc04d442adba59adda09f03d1d9c8c8067c8e2
Ricky Cook <ricky@dutyof.care>
2020-08-14 11:42:30 +1000

    Remove autoenv from zsh

--- files/zshrc.d/10-oh-my-zsh.zsh
+++ files/zshrc.d/10-oh-my-zsh.zsh
@@ -6,6 +6,6 @@ COMPLETION_WAITING_DOTS="true"  # Dots whilst waiting for completion
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git autoenv)
+plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
